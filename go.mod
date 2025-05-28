21f6e4ed865928f9d751cb00daa5fd689e4718b2
kunaaa123 <651463018@crru.ac.th>
2025-05-28 11:39:55 +0700

    feat: initial backend deployment setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module larkbot
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+type DeploymentInfo struct {
+	ENV         string `json:"env"`
+	Deployer    string `json:"deployer"`
+	ServiceName string `json:"serviceName"`
+	CommitMsg   string `json:"commitMsg"`
+}
+
+func sendToLark(info DeploymentInfo) error {
+	webhookURL := "https://open.larksuite.com/open-apis/bot/v2/hook/66a2d4a9-a7dd-47d3-a15a-c11c6f97c7f"
+
+	payload := map[string]interface{}{
+		"msg_type": "interactive",
+		"card": map[string]interface{}{
+			"header": map[string]interface{}{
+				"title": map[string]interface{}{
+					"content": "Backend Deployment Status",
+					"tag":     "plain_text",
+				},
+				"template": "blue",
+			},
+			"elements": []map[string]interface{}{
+				{
+					"tag": "div",
+					"text": map[string]interface{}{
+						"content": fmt.Sprintf("**ENV:** %s\n**Deployer:** %s\n**Service:** %s",
+							info.ENV, info.Deployer, info.ServiceName),
+						"tag": "lark_md",
+					},
+				},
+				{
+					"tag": "div",
+					"text": map[string]interface{}{
+						"content": fmt.Sprintf("**Commit Message:**\n%s", info.CommitMsg),
+						"tag":     "lark_md",
+					},
+				},
+			},
+		},
+	}
+
+	payloadBytes, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.Post(webhookURL, "application/json", bytes.NewBuffer(payloadBytes))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
+
+func main() {
+	http.HandleFunc("/deployment-info", func(w http.ResponseWriter, r *http.Request) {
+		info := DeploymentInfo{
+			ENV:         "DEV",
+			Deployer:    "rutchanai",
+			ServiceName: "tgth-backend-main",
+			CommitMsg:   "feat: backend deployment structure",
+		}
+
+		// ส่งข้อมูลไปยัง Lark
+		if err := sendToLark(info); err != nil {
+			log.Printf("Failed to send to Lark: %v", err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(info)
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Printf("Server running on port %s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
