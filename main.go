3b928d98a31d6548db7b20419e3aab8901e16a7b
kunaaa123 <651463018@crru.ac.th>
2025-05-30 00:58:03 +0700

    test Bot ที่ เเข็งที่สุด Mark 10.zero to branch backup-dev

--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"mime/multipart"
 	"net/http"
 	"os"
-	"path/filepath"
 )
 
 const (
@@ -16,6 +16,14 @@ const (
 	APP_ID       = "cli_a8b2c70af7389029"
 	APP_SECRET   = "QUbHQALAU0xrxWid9QU8Hb50wpY1wtwv"
 )
+
+type TokenResponse struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+	Data struct {
+		TenantAccessToken string `json:"tenant_access_token"`
+	} `json:"data"`
+}
 
 type GitHubPushEvent struct {
 	Repository struct {
@@ -29,39 +37,71 @@ type GitHubPushEvent struct {
 	} `json:"commits"`
 }
 
-func getTenantAccessToken() string {
-	payload := map[string]string{
-		"app_id":     APP_ID,
-		"app_secret": APP_SECRET,
+func sendLarkNotification(pushEvent *GitHubPushEvent, imageKey string) error {
+	lastCommit := pushEvent.Commits[0]
+	card := map[string]interface{}{
+		"msg_type": "interactive",
+		"card": map[string]interface{}{
+			"header": map[string]interface{}{
+				"title":    map[string]string{"tag": "plain_text", "content": "Backend Deployment"},
+				"template": "indigo",
+			},
+			"elements": []map[string]interface{}{
+				{
+					"tag": "img", "img_key": imageKey,
+					"mode": "fit_horizontal", "preview": true,
+				},
+				{
+					"tag": "div",
+					"text": map[string]interface{}{
+						"tag": "lark_md",
+						"content": fmt.Sprintf("**Service:** %s\n**Deployer:** %s\n**Message:** %s",
+							pushEvent.Repository.Name, lastCommit.Author.Name, lastCommit.Message),
+					},
+				},
+			},
+		},
 	}
-	payloadBytes, _ := json.Marshal(payload)
-	resp, _ := http.Post("https://open.larksuite.com/open-apis/auth/v3/tenant_access_token/internal",
-		"application/json", bytes.NewBuffer(payloadBytes))
-	defer resp.Body.Close()
 
-	var result struct {
-		TenantAccessToken string `json:"tenant_access_token"`
+	payload, err := json.Marshal(card)
+	if err != nil {
+		return err
 	}
-	json.NewDecoder(resp.Body).Decode(&result)
-	return result.TenantAccessToken
+
+	resp, err := http.Post(LARK_WEBHOOK, "application/json", bytes.NewBuffer(payload))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	return nil
 }
 
-func uploadImageToLark(filePath, token string) string {
-	file, _ := os.Open(filePath)
+func uploadImage(token string) (string, error) {
+	file, err := os.Open("./github_logo.png")
+	if err != nil {
+		return "", err
+	}
 	defer file.Close()
 
 	body := &bytes.Buffer{}
 	writer := multipart.NewWriter(body)
 	writer.WriteField("image_type", "message")
-	part, _ := writer.CreateFormFile("image", filepath.Base(filePath))
+	part, _ := writer.CreateFormFile("image", "github_logo.png")
 	io.Copy(part, file)
 	writer.Close()
 
-	req, _ := http.NewRequest("POST", "https://open.larksuite.com/open-apis/im/v1/images", body)
+	req, err := http.NewRequest("POST", "https://open.larksuite.com/open-apis/im/v1/images", body)
+	if err != nil {
+		return "", err
+	}
+
 	req.Header.Set("Authorization", "Bearer "+token)
 	req.Header.Set("Content-Type", writer.FormDataContentType())
 
-	resp, _ := http.DefaultClient.Do(req)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
 	defer resp.Body.Close()
 
 	var result struct {
@@ -69,96 +109,68 @@ func uploadImageToLark(filePath, token string) string {
 			ImageKey string `json:"image_key"`
 		} `json:"data"`
 	}
-	json.NewDecoder(resp.Body).Decode(&result)
-	return result.Data.ImageKey
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return "", err
+	}
+	return result.Data.ImageKey, nil
 }
 
-func handleGitHubWebhook(w http.ResponseWriter, r *http.Request) {
+func getTenantAccessToken() string {
+	data := map[string]string{
+		"app_id":     APP_ID,
+		"app_secret": APP_SECRET,
+	}
+
+	payload, err := json.Marshal(data)
+	if err != nil {
+		log.Printf("Failed to marshal token request: %v", err)
+		return ""
+	}
+
+	resp, err := http.Post(
+		"https://open.larksuite.com/open-apis/auth/v3/tenant_access_token/internal",
+		"application/json",
+		bytes.NewBuffer(payload),
+	)
+	if err != nil {
+		log.Printf("Failed to get token: %v", err)
+		return ""
+	}
+	defer resp.Body.Close()
+
+	var tokenResp TokenResponse
+	if err := json.NewDecoder(resp.Body).Decode(&tokenResp); err != nil {
+		log.Printf("Failed to decode token response: %v", err)
+		return ""
+	}
+
+	return tokenResp.Data.TenantAccessToken
+}
+
+func webhookHandler(w http.ResponseWriter, r *http.Request) {
 	var pushEvent GitHubPushEvent
-	json.NewDecoder(r.Body).Decode(&pushEvent)
+	if err := json.NewDecoder(r.Body).Decode(&pushEvent); err != nil {
+		http.Error(w, "Invalid payload", http.StatusBadRequest)
+		return
+	}
 
-	if len(pushEvent.Commits) > 0 {
-		token := getTenantAccessToken()
-		imageKey := uploadImageToLark("./github_logo.png", token)
-		lastCommit := pushEvent.Commits[0]
+	if len(pushEvent.Commits) == 0 {
+		return
+	}
 
-		payload := map[string]interface{}{
-			"msg_type": "interactive",
-			"card": map[string]interface{}{
-				"header": map[string]interface{}{
-					"title": map[string]interface{}{
-						"tag":     "plain_text",
-						"content": "Backend Deployment",
-					},
-					"template": "indigo",
-				},
-				"elements": []map[string]interface{}{
-					{
-						"tag":     "img",
-						"img_key": imageKey,
-						"mode":    "fit_horizontal",
-						"preview": true,
-					},
-					{
-						"tag": "div",
-						"fields": []map[string]interface{}{
-							{
-								"is_short": true,
-								"text": map[string]interface{}{
-									"tag":     "lark_md",
-									"content": fmt.Sprintf("<div class=\"box\" style=\"text-align: center\">**ENV**\nDEV</div>"),
-								},
-							},
-							{
-								"is_short": true,
-								"text": map[string]interface{}{
-									"tag":     "lark_md",
-									"content": fmt.Sprintf("<div class=\"box\" style=\"text-align: center\">**🤖 Deployer**\n%s</div>", lastCommit.Author.Name),
-								},
-							},
-						},
-					},
-					{
-						"tag": "div",
-						"text": map[string]interface{}{
-							"tag":     "lark_md",
-							"content": fmt.Sprintf("<div style=\"text-align: center\">**Service Name**\n%s</div>", pushEvent.Repository.Name),
-						},
-					},
-					{
-						"tag": "hr",
-					},
-					{
-						"tag": "div",
-						"text": map[string]interface{}{
-							"tag":     "lark_md",
-							"content": fmt.Sprintf("<div style=\"text-align: center\">**Commit Messages** 🤔\n• %s</div>", lastCommit.Message),
-						},
-					},
-					{
-						"tag": "action",
-						"actions": []map[string]interface{}{
-							{
-								"tag": "button",
-								"text": map[string]interface{}{
-									"content": "View repo",
-									"tag":     "plain_text",
-								},
-								"url":  fmt.Sprintf("https://github.com/%s", pushEvent.Repository.Name),
-								"type": "default",
-							},
-						},
-					},
-				},
-			},
-		}
+	imageKey, err := uploadImage(getTenantAccessToken())
+	if err != nil {
+		log.Printf("Failed to upload image: %v", err)
+		return
+	}
 
-		payloadBytes, _ := json.Marshal(payload)
-		http.Post(LARK_WEBHOOK, "application/json", bytes.NewBuffer(payloadBytes))
+	if err := sendLarkNotification(&pushEvent, imageKey); err != nil {
+		log.Printf("Failed to send notification: %v", err)
+		return
 	}
 }
 
 func main() {
-	http.HandleFunc("/git-webhook", handleGitHubWebhook)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/webhook", webhookHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
