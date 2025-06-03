a5f5c395e911953d37092a80c51caf7605a291d6
kunaaa123 <651463018@crru.ac.th>
2025-06-04 00:33:35 +0700

    refactor: clean up comments in GitHubPushEvent struct

--- cmd/internal/domain/github.go
+++ cmd/internal/domain/github.go
@@ -1,11 +1,11 @@
 package domain
 
 type GitHubPushEvent struct {
-	Ref        string `json:"ref"` // เพิ่ม field นี้
+	Ref        string `json:"ref"`
 	Repository struct {
 		Name     string `json:"name"`
 		FullName string `json:"full_name"`
-		HTMLURL  string `json:"html_url"` // เพิ่ม field นี้
+		HTMLURL  string `json:"html_url"`
 	} `json:"repository"`
 	Commits []struct {
 		Message string `json:"message"`
